47830b2c0e98627aaddf50f0da39c10776734521
Mrch.ej <89326316+ejeee@users.noreply.github.com>
2023-07-10 11:56:28 +0700

    Add files via upload

---
+++ FinalExam/js/questions.js
@@ -0,0 +1,40 @@
+let questions = [
+  {
+    numb: 1,
+    question: "An entity can only have one Primary UID. True or False?",
+    answer: "True",
+    options: ["True", "False"],
+  },
+  {
+    numb: 2,
+    question:
+      "If an entity has no attribute suitable to be a Primary UID, we can create an artificial one. True or False?",
+    answer: "True",
+    options: ["True", "False"],
+  },
+  {
+    numb: 3,
+    question:
+      "There is no limit to how many columns can make up an entity's UID. True or False?",
+    answer: "True",
+    options: ["True", "False"],
+  },
+  {
+    numb: 4,
+    question:
+      "A unique identifier can only be made up of one attribute. True or False?",
+    answer: "False",
+    options: ["True", "False"],
+  },
+  {
+    numb: 5,
+    question: "When is an entity in 2nd Normal Form?",
+    answer: "When all non-UID attributes are dependent upon the entire UID.",
+    options: [
+      "When all non-UID attributes are dependent upon the entire UID.",
+      "When attributes with repeating or multi-values are present.",
+      "When no attritibutes are mutually independent and all are fully dependent on the primary key.",
+      "None of the Above.",
+    ],
+  },
+];
